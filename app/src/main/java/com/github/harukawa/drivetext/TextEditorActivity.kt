0f62158ef5fea3605b02ef2cf648f75496cd00f0
harukawa <harukawa4k@gmail.com>
2020-05-30 23:08:44 +0900

    Removed sending function from TextEditor All communication with Drive is done with the sync button on the main screen, so the unnecessary TextEditor communication functions and buttons have been deleted.

--- app/src/main/java/com/github/harukawa/drivetext/TextEditorActivity.kt
+++ app/src/main/java/com/github/harukawa/drivetext/TextEditorActivity.kt
@@ -4,26 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.Scopes
-import com.google.android.gms.common.api.Scope
-import com.google.api.client.extensions.android.http.AndroidHttp
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
-import com.google.api.client.http.FileContent
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.DateTime
-import com.google.api.services.drive.Drive
-import com.google.api.services.drive.DriveScopes
 import io.github.karino2.listtextview.ListTextView
 import kotlinx.android.synthetic.main.activity_text_editor.*
 import kotlinx.coroutines.*
@@ -36,8 +22,6 @@ typealias DriveFile = com.google.api.services.drive.model.File
 class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     companion object {
-        private const val REQUEST_UPLOAD = 1
-        private const val REQUEST_UPDATE = 2
         private const val REQUEST_EDIT_CELL_CODE=3
     }
 
@@ -72,6 +56,7 @@ class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
             val tsf = SimpleDateFormat("yyyy-MM-dd-HHmmss")
             titleText.setText(tsf.format(Date()) + ".txt")
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -80,8 +65,8 @@ class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_sent -> {
-            sentFile()
+        R.id.action_save -> {
+            saveFile()
             true
         }
         else -> {
@@ -95,77 +80,6 @@ class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                 return
             }
         }
-
-        when(requestCode) {
-            REQUEST_UPLOAD -> {
-                //https://developers.google.com/api-client-library/java/google-api-java-client/media-upload
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    /*
-                    Upload a local file to GoogleDrive.
-                    Upload a file that has no files on GoogleDrive and exists only locally.
-                    Use the file name and path to upload GoogleDrive.
-                    The id is also used for updating.
-                    Since there is no id in the file before uploading,
-                    empty characters are registered in the database.
-                    Since id is specified by GoogleDrive,
-                    after uploading it is obtained based on the file name.
-                    After obtaining the id, use the id to determine the file.
-                     */
-
-
-                    val drive = setDriveConnect(data, this)
-
-                    // Get local file information to upload
-                    val localFile = database.getLocalFile(dbId)
-                    val path = applicationContext.filesDir.path + "/_" + localFile.name
-
-                    launch {
-                        uploadFile(drive, path, localFile.name)
-
-                        // Get GoogleDriveID of uploaded file and upload time
-                        val (id, date) = getFileIdAndDate(drive, localFile)
-
-                        // Update database information
-                        val newLocalFile = LocalFile(localFile.name, id, 0L)
-                        database.updateDriveEntry(dbId, newLocalFile, driveDate = date)
-
-                        // Add GoogleDrive ID to local file name to check if uploading or not.
-                        renameFile("_" + localFile.name, id + "_" + localFile.name)
-                        finish()
-                    }
-                } else {
-                    Log.d("FailureDriveConnect","failed upload file")
-                }
-            }
-            REQUEST_UPDATE -> {
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    /*
-                    Overwrite the file existing in GoogleDrive to the local file.
-                    Unlike upload, it also uses id.
-                    Since the id is not changed,
-                    Only the update time is obtained from GoogleDrive.
-                     */
-
-                    val drive = setDriveConnect(data, this)
-                    val localFile = database.getLocalFile(dbId)
-                    val path = applicationContext.filesDir.path + "/" + localFile.fileName
-
-                    launch {
-                        updateFile(drive, path, localFile)
-
-                        // Get upload time
-                        val (_, date) = getFileIdAndDate(drive, localFile)
-
-                        // Update database information
-                        database.updateDriveEntry(dbId, localFile, driveDate = date)
-                        finish()
-                    }
-                } else {
-                    Log.d("FailureDriveConnect","failed update file")
-                }
-            }
-        }
-
         super.onActivityResult(requestCode, resultCode, data)
     }
 
@@ -187,19 +101,6 @@ class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         dbId = database.getId(title)
     }
 
-    fun sentFile() {
-        saveFile()
-        val localFile = database.getLocalFile(dbId)
-        val request = if(localFile.fileId == "") REQUEST_UPLOAD else REQUEST_UPDATE
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestScopes(Scope(DriveScopes.DRIVE)).requestEmail()
-            .build()
-
-        val client = GoogleSignIn.getClient(this, gso)
-        startActivityForResult(client.signInIntent, request)
-    }
-
     fun readFile() {
         val localFile = database.getLocalFile(dbId)
         val fileName = localFile.fileName
@@ -214,100 +115,4 @@ class TextEditorActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         input.close()
     }
 
-    fun renameFile(old : String, new: String) {
-        val s = this.filesDir
-        val from = File(s, old)
-        val to = File(s, new)
-        if(from.exists()) {
-            from.renameTo(to)
-        }
-    }
-
-    fun setDriveConnect(data: Intent, context: Context): Drive {
-        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-        val googleAccount : GoogleSignInAccount = task.result!!
-        val credential : GoogleAccountCredential = GoogleAccountCredential.usingOAuth2(
-            context, listOf(DriveScopes.DRIVE_FILE, Scopes.DRIVE_APPFOLDER)
-        )
-        credential.selectedAccount = googleAccount.account
-        val googleDriveService = Drive.Builder(
-            AndroidHttp.newCompatibleTransport(),
-            JacksonFactory.getDefaultInstance(),
-            credential)
-            .setApplicationName(context.getString(R.string.app_name))
-            .build()
-        return googleDriveService
-    }
-
-    suspend fun getFileIdAndDate(googleDriveService: Drive, localFile: LocalFile) : Pair<String, Date> {
-        val pageToken : String? = null
-        var fileDate: DateTime = DateTime(0)
-        var fileId = ""
-        withContext(Dispatchers.IO) {
-
-
-            val result = googleDriveService.files().list().apply {
-                q = "mimeType='text/plain'"
-                spaces = "drive"
-                fields = "nextPageToken, files(id, name, modifiedTime)"
-                this.pageToken = pageToken
-            }.execute()
-            for (file in result.files) {
-                if (localFile.isMatch(file)) {
-                    fileId = file.id
-                    fileDate = file.modifiedTime
-                }
-            }
-        }
-        return Pair(fileId, Date(fileDate.value))
-    }
-
-    suspend fun uploadFile(googleDriveService: Drive, filePath : String, fileName: String) {
-        // Uploading file refers to https://developers.google.com/drive/api/v3/manage-uploads
-        // To make FileContent with file's URI, the pdf file is saved as a temp file.
-        val type = "text/plain"
-
-        // If you have set the Google Drive folder ID, get that ID.
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val parentId = prefs.getString("drive_parent_path", "")
-
-        // Upload file settings.
-        val blobMd = FileContent(type, File(filePath))
-        val targetDriveFile = DriveFile()
-        targetDriveFile.name = fileName
-        if(parentId != "") targetDriveFile.parents = arrayListOf(parentId)
-
-        withContext(Dispatchers.IO) {
-            try {
-                googleDriveService.files().create(targetDriveFile, blobMd)
-                    .setFields("id, mimeType, modifiedTime")
-                    .execute()
-            } catch (e: UserRecoverableAuthIOException) {
-                // Only the first time an error appears, so take out the intent and upload it again.
-                Log.d("GoogleSign", "Error first Lognin :${e.toString()}")
-                val mIntent = e.intent
-                startActivityForResult(mIntent, REQUEST_UPLOAD)
-            }
-        }
-    }
-
-    suspend fun updateFile(googleDriveService: Drive, filePath: String, localFile: LocalFile) {
-        // Uploading file refers to https://developers.google.com/drive/api/v3/manage-uploads
-        // Update file settings.
-        val blobMd = FileContent("text/plain", File(filePath))
-        val targetDriveFile = DriveFile()
-        targetDriveFile.name = localFile.name
-
-        withContext(Dispatchers.IO) {
-            try {
-                googleDriveService.files().update(localFile.fileId, targetDriveFile, blobMd)
-                    .execute()
-            } catch (e: UserRecoverableAuthIOException) {
-                // Only the first time an error appears, so take out the intent and upload it again.
-                Log.d("GoogleSign", "Error first Login :${e.toString()}")
-                val mIntent = e.intent
-                startActivityForResult(mIntent, REQUEST_UPDATE)
-            }
-        }
-    }
 }
